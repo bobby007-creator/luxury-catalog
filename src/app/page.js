6de28e596a9239059482104221a0c908d7153da3
bobby007-creator <bobby.special87@gmail.com>
2026-06-14 23:41:23 +0530

    Bypass Vercel fs trace

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,7 @@
-import fs from 'fs';
-import path from 'path';
 import FlipbookWrapper from '@/components/FlipbookWrapper';
+import catalogData from '../../data/catalogData.json';
 
 export default async function Home() {
-  const filePath = path.join(process.cwd(), 'data', 'catalogData.json');
-  const jsonData = fs.readFileSync(filePath, 'utf8');
-  const catalogData = JSON.parse(jsonData);
-
   return (
     <main style={{ width: '100vw', height: '100vh', backgroundColor: '#fdfaf6' }}>
       <FlipbookWrapper catalogData={catalogData} />
