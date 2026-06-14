37746f24cd97c6449b8bd3619fba64443932efe9
bobby007-creator <bobby.special87@gmail.com>
2026-06-15 02:11:23 +0530

    Create test-site.js

---
+++ test-site.js
@@ -0,0 +1,15 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  
+  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  page.on('pageerror', error => console.log('PAGE ERROR:', error.message));
+  
+  console.log('Navigating...');
+  await page.goto('http://localhost:3000', { waitUntil: 'networkidle' });
+  console.log('Done.');
+  
+  await browser.close();
+})();
